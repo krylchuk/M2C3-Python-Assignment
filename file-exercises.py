1a3eb4c3196a22d72a10e088437b2c748b7f92bf
krylchuk <krultchuk@gmail.com>
2024-03-07 23:19:16 +0100

    first commit

---
+++ file-exercises.py
@@ -0,0 +1,50 @@
+# Exercise 1
+name = 'Ivan'
+number_of_free_seats = 27
+exercise_completed = False
+names = ['Ivan', 'Ricardo', 'Felix', 'Sara']
+
+print(name)
+print(number_of_free_seats)
+print(exercise_completed)
+print(names)
+
+# Exercise 2
+letters = name[:3]
+print(letters)
+
+# Exercise 3
+first_element = names[0]
+print(first_element)
+
+# Exercise 4
+number_of_seats = number_of_free_seats + 10
+print(number_of_seats)
+
+# Exercise 5
+print(names[-1])
+
+# Exercise 6
+names_one = 'harry,alex,susie,jared,gail,conner'
+list_of_names_one = names_one.split(',')
+print(list_of_names_one)
+
+# Exercise 7
+sentence = 'the quick brown'
+first_word = sentence.split()[0]
+first_word = first_word.upper()
+sentence_two = first_word + sentence[3:]
+print(sentence_two)
+
+# Exercise 8
+buy_tickets = f'There are {number_of_free_seats} free seats on the left tribune'
+print(buy_tickets)
+
+# Exercise 9
+greeting = "hello world"
+print(greeting)
+
+
+
+
+
